6edd15cfb28b4f22e0274619594194378ed838c3
Tracy Vierra <tracyvierra@yahoo.com>
2021-05-04 15:29:08 -0700

    copied dice_fucntions.py to make changes

--- dice_functions copy.py
+++ dice_functions copy.py
@@ -5,22 +5,22 @@ def dice_roll():
     NUM_DICE = num_dice = int(input("Number of dice to roll: ")) 
     MIN_SIDES = 1
     MAX_SIDES = NUM_SIDES
-    total = 0
+    j = 0
     print("")
 
     for i in range(NUM_DICE):
         die_roll = int(random.randint(MIN_SIDES,NUM_SIDES))
         print("Your " + str(NUM_SIDES) + " sided roll is: " + str(die_roll))
-        total += die_roll
+        j = die_roll + j
     
-     
+    total = j 
     print("")   
     print("Total = " + str(total))
     print("")
     
 def roll_again():
     roll_again = input("roll again? ")
-    while roll_again == "y":
+    if roll_again == "y":
         print("")
         dice_roll()
         roll_again = input("roll again? ")
@@ -34,7 +34,7 @@ def roll_again():
         print("")
         
 
-print("")
+
 print("Welcome to Tracy's Dice roller app!")
 print("")
 

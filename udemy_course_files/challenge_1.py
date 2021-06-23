55e3a6a309f9660f5c7518cce7c9d94171353f8d
Tracy Vierra <tracyvierra@yahoo.com>
2021-06-23 14:13:14 -0700

    added formatting.

--- udemy_course_files/challenge_1.py
+++ udemy_course_files/challenge_1.py
@@ -3,10 +3,13 @@ check if person is the right age to go on an 18-30 holiday
 (they must be over 18 and under 30)
 If they are eligible print a welcome message, if not a polit message refusing them entry.
 '''
+print(" ")
 name = input("Please enter your name: ")
 age = int(input("Please enter your age: "))
 
 if age <= 17 or age >= 30:
     print("Sorry, {0} you are not in the correct age range for this Holiday trip.".format(name))
+    print(" ")
 else:
     print("Welcome to the Holiday Trip!   Please follow Gopher and Julie ...")
+    print(" ")

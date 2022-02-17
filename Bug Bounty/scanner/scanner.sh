ef74e90eaa20370e2bc537594048676dce027c3f
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-17 08:32:27 -0800

    adding scanner folder, scanner.sh , scan.lib

---
+++ Bug Bounty/scanner/scanner.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source ./scan.lib
+PATH_TO_DIRSEARCH="/Users/vickieli/tools/dirsearch"
+getopts "m:" OPTION
+MODE=$OPTARG
+for i in "${@:$OPTIND:$#}"
+do
+ DOMAIN=$i
+ DIRECTORY=${DOMAIN}_recon
+ echo "Creating directory $DIRECTORY."
+ mkdir $DIRECTORY
+
+ case $MODE in
+ nmap-only)
+ nmap_scan
+ ;;
+ dirsearch-only)
+ dirsearch_scan
+ ;; 
+ crt-only) 
+ crt_scan
+ ;;
+ *) 
+ nmap_scan
+ dirsearch_scan
+ crt_scan
+ ;; 
+ esac
+ echo "Generating recon report for $DOMAIN..."
+ TODAY=$(date)
+ echo "This scan was created on $TODAY" > $DIRECTORY/report
+ if [ -f $DIRECTORY/nmap ];then
+ echo "Results for Nmap:" >> $DIRECTORY/report
+ grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
+ fi
+ if [ -f $DIRECTORY/dirsearch ];then
+ echo "Results for Dirsearch:" >> $DIRECTORY/report
+ cat $DIRECTORY/dirsearch >> $DIRECTORY/report
+ fi
+ if [ -f $DIRECTORY/crt ];then
+ echo "Results for crt.sh:" >> $DIRECTORY/report
+ jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/report
+ fi
+done

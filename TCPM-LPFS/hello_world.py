fd6d73dbfbb8f960db5137540cbea14dc99b3cf9
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-24 13:05:32 -0800

    added a few more examples to hello world

--- TCPM-LPFS/hello_world.py
+++ TCPM-LPFS/hello_world.py
@@ -2,3 +2,16 @@ print("Hello World!")
 
 # square root without using a math module.
 print(144**(1/2))
+
+if 144**(1/2) == 12:
+    print("The square root of 144 is 12.")
+else:
+    print("The square root of 144 is not 12.")
+
+# to check if a number is even or odd:
+
+num=int(input("Enter a number: "))
+if num % 2 == 0:    # if the number is even a 0 is returned, if the number is odd a 1 is returned.
+    print(f'{num} is even')
+else:
+    print(f'{num} is odd')

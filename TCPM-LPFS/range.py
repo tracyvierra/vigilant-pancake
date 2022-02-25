593025ba5ca2b52ef943595deda430467699346d
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-25 10:41:09 -0800

    added range.py

---
+++ TCPM-LPFS/range.py
@@ -0,0 +1,26 @@
+# Author: Tracy Vierra
+# Date Created: 2/25/2022
+# Date Modified: 2/25/2022
+
+# Description:
+
+# Usage:
+
+
+numbers = list(range(10))
+print(numbers)
+
+numbers = list(range(3,8))
+print(numbers)
+
+numbers = list(range(1,30))
+print(numbers)
+
+numbers = list(range(1,30,3))
+print(numbers)
+
+numbers = list(range(1,30,2))
+print(numbers)
+
+numbers = list(range(2,30,2))
+print(numbers)

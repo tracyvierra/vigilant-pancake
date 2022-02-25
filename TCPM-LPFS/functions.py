d89d8571bebcc310e8ae3880064cc62499b37dc4
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-25 11:51:56 -0800

    adding function with return to functions.py

--- TCPM-LPFS/functions.py
+++ TCPM-LPFS/functions.py
@@ -42,3 +42,19 @@ add(5,6)
 add(7,8)
 add(9,10)
 add(11,12)
+
+print(" ")
+
+# Function definition is here
+def sum( arg1, arg2 ):
+   # Add both the parameters and return them."
+   total = arg1 + arg2
+   print("Inside the function : ", total)
+   return total;
+
+# Now you can call sum function
+total = sum( 10, 20 );
+print("Outside the function : ", total)
+
+print(" ")
+

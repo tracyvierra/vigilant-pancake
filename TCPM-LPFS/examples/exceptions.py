9ad8f17958571cb010e424d399dea94db0fc5e60
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-25 13:49:09 -0800

    updated exceptions.py

--- TCPM-LPFS/examples/exceptions.py
+++ TCPM-LPFS/examples/exceptions.py
@@ -7,7 +7,21 @@
 # Usage:
 
 # to get around errors that you can anticipate use a try and except block
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
 
+x = float(input("Enter a number: "))
+y = float(input("Enter the number to divide by: "))
+
+divide(x, y)
+
+
+'''
 try:
     a = 20
     b = 0
@@ -21,3 +35,4 @@ finally:
     # do something regardless of the error
     print("I'm done")
 
+'''

3b82e944de433f0551efe0e3e568346e8824ffd4
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-25 10:56:29 -0800

    adding files

---
+++ TCPM-LPFS/for_loop.py
@@ -0,0 +1,32 @@
+# Author: Tracy Vierra
+# Date Created: 2/25/2022
+# Date Modified: 2/25/2022
+
+# Description:
+
+# Usage:
+
+for x in range(1, 11):
+    print(x)
+
+print()
+
+for x in range(11, 0, -1):
+    print(x, end=" ")
+
+print()
+
+for x in range(1, 11, 2):
+    print(x, end=" ")
+
+print()
+
+for x in range(2, 11, 2):
+    print(x, end=" ")
+
+print()
+
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+	    print(x, end=" ")
---
+++ TCPM-LPFS/functions.py
@@ -0,0 +1,15 @@
+# Author: Tracy Vierra
+# Date Created: 2/25/2022
+# Date Modified: 2/25/2022
+
+# Description:
+
+# Usage:
+
+def function1():
+    print("This is function 1")
+
+
+function1()
+
+

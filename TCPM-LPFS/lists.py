c31f8769fd9280c709c040c852faf2813adc03d6
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-25 10:33:27 -0800

    renamed script_2.py to lists.py

---
+++ TCPM-LPFS/lists.py
@@ -0,0 +1,21 @@
+# Author: Tracy Vierra
+# Date Created: 2/24/2022
+# Date Modified: 2/25/2022
+
+# Description:
+
+# Usage:
+
+fruits = ["Apple", "Mango", "Peach", "Orange"]
+fruits.append("Grape")
+print(fruits)
+
+fruits.remove("Mango")
+print(fruits)
+
+fruits.index("Orange")
+
+print(len(fruits))
+
+fruits.insert(2, "Strawberry")
+print(fruits)
--- TCPM-LPFS/script_2.py
+++
@@ -1,7 +0,0 @@
-# Author: Tracy Vierra
-# Date Created: 2/24/2022
-# Date Modified: 2/24/2022
-
-# Description:
-
-# Usage:

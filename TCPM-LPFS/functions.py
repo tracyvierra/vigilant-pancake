8306e2bdddb8010d2f4753710cecf5d629ecd613
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-25 11:44:06 -0800

    adding functions to functions.py

--- TCPM-LPFS/functions.py
+++ TCPM-LPFS/functions.py
@@ -12,9 +12,10 @@ def function1():
 def square_root(x):
     return x**0.5
 
-
+print(" ")
 
 function1()
+print(" ")
 print(square_root(2))
 print(square_root(6))
 print(square_root(9))
@@ -28,3 +29,14 @@ print(square_root(100))
 print(square_root(121))
 print(square_root(144))
 
+# function arguments
+def add(a,b):
+    print(a+b)
+    return a+b
+print(" ")
+
+add(3,4)
+add(5,6)
+add(7,8)
+add(9,10)
+add(11,12)

f22919b749d53c064087295bad3de631b68c3143
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-25 12:14:12 -0800

    adding modules.py as an emaple of importing a module

---
+++ TCPM-LPFS/modules.py
@@ -0,0 +1,29 @@
+# Author: Tracy Vierra
+# Date Created: 2/25/2022
+# Date Modified: 2/25/2022
+
+# Description:
+
+# Usage:
+
+import random
+
+roll = random.randint(1,6)
+print(roll)
+
+
+
+print(" ")
+
+roll1 = random.randint(1,12)
+print(roll1)
+
+
+print(" ")
+
+roll2 = random.randint(1,20)
+print(roll2)
+
+
+print(" ")
+

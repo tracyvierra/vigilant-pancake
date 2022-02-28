055be8fbcd40156a7efe92ed16bed479abdf63f2
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-28 14:10:17 -0800

    updating generators.py

---
+++ TCPM-LPFS/generators.py
@@ -0,0 +1,18 @@
+# Author: Tracy Vierra
+# Date Created: 2/28/2022
+# Date Modified: 2/28/2022
+
+# Description:
+
+# Usage:
+
+def function():
+    counter = 0
+    while counter < 5:
+        yield counter
+	counter += 1
+
+for x in function():
+    print(x)
+
+	

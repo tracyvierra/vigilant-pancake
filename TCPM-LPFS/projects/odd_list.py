acbc6dcfbd34b926e4ca9cc428a87f5e5f4f6563
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-28 11:35:08 -0800

    moved 2 files into projects folder

---
+++ TCPM-LPFS/projects/odd_list.py
@@ -0,0 +1,15 @@
+# Author: Tracy Vierra
+# Date Created: 2/28/2022
+# Date Modified: 2/28/2022
+
+# Description:
+
+# Usage:
+
+odd_num = []
+for x in range(1,100,2):
+  odd_num.append(x)
+
+print(odd_num)
+
+

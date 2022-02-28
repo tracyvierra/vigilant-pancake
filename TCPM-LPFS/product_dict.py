2521becd55955cb870d16f57aaa80815322a2dc9
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-28 11:23:18 -0800

    added product_dict.py and updated notes.

---
+++ TCPM-LPFS/product_dict.py
@@ -0,0 +1,23 @@
+# Author: Tracy Vierra
+# Date Created: 2/28/2022
+# Date Modified: 2/28/2022
+
+# Description:
+
+# Usage:
+
+products = {"product 1": {"price": 10, "stock": 5}, "product 2": {"price": 20, "stock": 10}, "product 3": {"price": 30, "stock": 15}, "product 4": {"price": 60, "stock": 15}, "product 5": {"price": 70, "stock": 15}}
+
+print("Product List: ")
+print(" ")
+
+for key in products.keys():
+  print(key)
+
+print(" ")
+
+prod = input("Please enter a product to see the price and number of stock available: ")
+
+print(products.get(prod))
+
+

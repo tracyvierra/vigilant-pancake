c89e27e46af4d7bca6868237c43519f5ca3267d3
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-01 09:26:12 -0800

    added class_example.py and method_example.py

---
+++ TCPM-LPFS/class_example.py
@@ -0,0 +1,18 @@
+# Author: Tracy Vierra
+# Date Created: 3/1/2022
+# Date Modified: 3/1/2022
+
+# Description:
+
+# Usage:
+class Teddy:
+	quantity = 200
+
+teddy1 = Teddy()
+print(teddy1.quantity)
+teddy2 = Teddy()
+print(teddy2.quantity)
+teddy1.quantity = 300
+print(teddy1.quantity)
+print(teddy2.quantity)
+
---
+++ TCPM-LPFS/method_example.py
@@ -0,0 +1,7 @@
+# Author: Tracy Vierra
+# Date Created: 3/1/2022
+# Date Modified: 3/1/2022
+
+# Description:
+
+# Usage:

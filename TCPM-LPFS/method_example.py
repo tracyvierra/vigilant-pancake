480af3245b38ae46919c5363157ccb60bd1e2112
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-01 09:45:26 -0800

    updated method_example.py

--- TCPM-LPFS/method_example.py
+++ TCPM-LPFS/method_example.py
@@ -5,3 +5,23 @@
 # Description:
 
 # Usage:
+
+
+class Teddy:
+	quantity = 200				# class attribute	
+	def __init__(self, color, name):	# constructor
+		self.color = color		# instance attribute
+		self.name = name		# instance attribute
+
+
+	def change_color(self, color):		# instance method
+		print("This is a method")
+		self.color = "White"
+
+teddy1 = Teddy("Red", "Teddy")
+print(teddy1.name)
+print(teddy1.color)
+
+teddy1.change_color("Blue")
+print(teddy1.name)
+print(teddy1.color)

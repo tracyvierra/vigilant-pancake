20108fdead5b96621fe7428abea85b8b6cb97375
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-01 15:02:37 -0800

    added multi-lever_inheritance.py and updated multiple_inheritance_example.py

---
+++ TCPM-LPFS/examples/multi-level_inheritance.py
@@ -0,0 +1,9 @@
+# Author: Tracy Vierra
+# Date Created: 3/1/2022
+# Date Modified: 3/1/2022
+
+# Description:
+
+# Usage:
+
+
---
+++ TCPM-LPFS/examples/multiple_inheritance_example.py
@@ -0,0 +1,70 @@
+# Author: Tracy Vierra
+# Date Created: 3/1/2022
+# Date Modified: 3/1/2022
+
+# Description:
+
+# Usage:
+
+# class Student:              # base class
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def get_data(self):
+#         self.name = input("Enter name: ")
+#         self.age = input("Enter age: ")
+
+#     def display(self):
+#         print(self.name)
+#         print(self.age)
+
+# class ScienceStudent(Student):  # derived class
+#     def science(self):
+#         print("this is a science method")
+
+
+    # def __init__(self, name, age):
+    #     self.name = name
+    #     self.age = age
+    #     self.major = "Physics"
+
+    # def get_data(self):
+    #     self.name = input("Enter name: ")
+    #     self.age = input("Enter age: ")
+
+    # def display(self):
+    #     print(self.name)
+    #     print(self.age)
+    #     print(self.major)
+
+
+# student1 = Student("", "")
+# student1.get_data()
+# student1.display()
+
+# a = ScienceStudent("", "")
+# a.science()
+# a.get_data()
+# a.display()
+
+
+class A:
+    def a_method(self):
+        print("A method")
+
+class B:
+    def b_method(self):
+        print("B method")
+
+class C(A,B):
+    def c_method(self):
+        print("C method")
+
+
+c_object = C()
+c_object.a_method()
+c_object.b_method()
+c_object.c_method()
+
+

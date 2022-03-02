f43bde65b40f9bee9a1fedb45a79757cc317af12
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-02 11:14:14 -0800

    added their solutions to the projects

--- TCPM-LPFS/projects/op_overload_project.py
+++ TCPM-LPFS/projects/op_overload_project.py
@@ -22,3 +22,22 @@ oddity1 = Oddity(1, 2)
 oddity2 = Oddity(3, 4)
 print(oddity1 + oddity2)
 print(oddity1 * oddity2)
+
+
+# Their solution: 
+
+class Number:
+
+    def __init__(self,x):
+        self.x = x
+
+    def __mul__(self, other):
+        x = self.x + other.x
+        return x
+
+number_a = Number(3)
+
+number_b = Number(4)
+
+print(number_a * number_b)
+
--- TCPM-LPFS/projects/super_class.py
+++ TCPM-LPFS/projects/super_class.py
@@ -65,4 +65,54 @@ print(Computer.Desktop.displayspecs(display="35.9"))
 print(Computer.Desktop.getspecs(cpu="i9", ram="64GB", hdd="3TB", display="26.6"))
 print(Computer.Laptop.getspecs(cpu="i9", ram="32GB", hdd="2TB", display="17.6", weight="3"))
 
+# Their solution: 
+
+class Computer:
+    def __init__(self, ram, memory, processor):
+        self.ram = ram
+        self.memory = memory
+        self.processor = processor
+
+    def getspecs(self):
+        print('Please enter the details')
+        self.ram = input('Enter Ram Size: ')
+        self.memory = input('Memory size: ')
+        self.processor = input('Enter processor: ')
+
+    def displayspecs(self):
+        print('Here are the specs of the computer')
+        print(' Ram size is: ' + self.ram + ' Memory size is: ' + self.memory + ' processor is: ' + self.processor)
+
+
+class Desktop(Computer):
+    def __init__(self, casecolor):
+        self.casecolor = casecolor
+
+    def get_case_details(self):
+        self.casecolor = input('Enter case color')
+
+    def put_case_details(self):
+        print('case color is: ' + self.casecolor)
+
+
+class Laptop(Computer):
+    def __init__(self, weight):
+        self.weight = weight
+
+    def getweight(self):
+        self.weight = input('Enter weight')
+
+    def displayweight(self):
+        print('weight is: ' + self.weight)
+
+
+comp = Laptop('');
+
+comp.getspecs()
+
+comp.getweight()
+
+comp.displayspecs()
+
+comp.displayweight()
 

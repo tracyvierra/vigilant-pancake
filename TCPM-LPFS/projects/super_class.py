2a243c3520d3a70b25a75e74439aa55200adf403
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-02 10:54:16 -0800

    added super_class.py to projects folder

---
+++ TCPM-LPFS/projects/super_class.py
@@ -0,0 +1,68 @@
+# Author: Tracy Vierra
+# Date Created: 3/2/2022
+# Date Modified: 3/2/2022
+
+# Description:
+
+# Usage:
+
+class Computer:
+	class Desktop:
+		def __init__(self, cpu, ram, hdd, display):
+			self.cpu = cpu
+			self.ram = ram
+			self.hdd = hdd
+			self.display = display
+		
+		def getspecs(cpu, ram, hdd, display):
+	            return "CPU: {}, RAM: {}, HDD: {}, DISPLAY: {}".format( cpu, ram, hdd, display )
+
+		def displayspecs(display):
+		    return "DISPLAY: {}".format(display)
+
+		def __str__(self):
+			return "CPU: {}, RAM: {}, HDD: {}".format(self.cpu, self.ram, self.hdd)
+
+	class Laptop:
+		def __init__(self, cpu, ram, hdd, display, weight):
+			self.cpu = cpu
+			self.ram = ram
+			self.hdd = hdd
+			self.display = display
+			self.weight = weight
+
+		def getspecs(cpu, ram, hdd, display, weight):
+	            return "CPU: {}, RAM: {}, HDD: {}, DISPLAY: {}, WEIGHT: {}".format(cpu, ram, hdd, display, weight)
+
+		def displayspecs(display):
+	            return "DISPLAY: {}".format(display)
+
+		def __str__(self):
+		    return "CPU: {}, RAM: {}, HDD: {}, WEIGHT: {}".format(self.cpu, self.ram, self.hdd, self.weight)
+
+	# def getspecs(self, cpu, ram, hdd, display, weight):
+	#         return "CPU: {}, RAM: {}, HDD: {}, DISPLAY: {}, WEIGHT: {}".format(self.cpu, self.ram, self.hdd, self.display, self.weight)
+
+	# def displayspecs(display):
+	# 	return "DISPLAY: {}".format(display)
+
+	def __init__(self, cpu, ram, hdd, display, weight):
+		self.cpu = cpu
+		self.ram = ram
+		self.hdd = hdd
+		self.display = display
+		self.weight = weight
+
+desktop = Computer.Desktop("i7", "16GB", "1TB", "15.6")
+laptop = Computer.Laptop("i5", "8GB", "500GB", "15.6", "2.5") 
+
+print(desktop)
+print(laptop)
+print(Computer.Laptop(cpu="i9", ram="32GB", hdd="2TB", display="17.6", weight="3.5"))
+print(Computer.Desktop(cpu="i9", ram="48GB", hdd="4TB", display="26.6"))
+print(Computer.Laptop.displayspecs(display="19.6"))
+print(Computer.Desktop.displayspecs(display="35.9"))
+print(Computer.Desktop.getspecs(cpu="i9", ram="64GB", hdd="3TB", display="26.6"))
+print(Computer.Laptop.getspecs(cpu="i9", ram="32GB", hdd="2TB", display="17.6", weight="3"))
+
+

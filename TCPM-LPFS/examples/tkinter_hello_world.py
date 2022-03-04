4f34b577afc5236a0a88efdb17a3074676df648e
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-03 16:00:17 -0800

    added tkinter_hello_world.py

---
+++ TCPM-LPFS/examples/tkinter_hello_world.py
@@ -0,0 +1,16 @@
+# Author: Tracy Vierra
+# Date Created: 3/3/2022
+# Date Modified: 3/3/2022
+
+# Description:
+
+# Usage:
+
+from tkinter import *
+
+root = Tk()
+
+label1 = Label(root, text="Hello World!")			# Create a label widget
+label1.pack()							# Pack it into the window
+root.mainloop()
+

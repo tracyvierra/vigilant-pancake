4be06e9acd4f352871a051197b1dba481150ee08
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-04 10:27:22 -0800

    updated tkinter_calc

--- TCPM-LPFS/projects/tkinter_calc/tkinter_calc.py
+++ TCPM-LPFS/projects/tkinter_calc/tkinter_calc.py
@@ -18,6 +18,13 @@ def get_variables(num):
 	display.insert(i, num)
 	i += 1
 
+def clear_all():	
+	display.delete(0, END)	
+
+def del_char():
+	length = len(display.get())	
+	display.delete(length - 1)	
+
 
 
 # adding the iput field
@@ -25,6 +32,7 @@ display = Entry(root)					# Create an Entry widget
 display.grid(row=1, column=0, columnspan=6, sticky=W+E)	# Add it to the window
 
 # adding the first row buttons
+
 # button_1 = Button(root, text="1", padx=20, pady=20)
 # button_1.grid(row=2, column=0)
 # button_2 = Button(root, text="2", padx=20, pady=20)
@@ -38,6 +46,7 @@ Button(root, text="3", command=lambda :get_variables(3), padx=5, pady=5).grid(ro
 
 
 # adding the second row buttons
+
 # button_4 = Button(root, text="4", padx=20, pady=20)
 # button_4.grid(row=3, column=0)
 # button_5 = Button(root, text="5", padx=20, pady=20)
@@ -51,6 +60,7 @@ Button(root, text="6", command=lambda :get_variables(6), padx=5, pady=5).grid(ro
 
 
 # adding the third row buttons
+
 # button_7 = Button(root, text="7", padx=20, pady=20)
 # button_7.grid(row=4, column=0)
 # button_8 = Button(root, text="8", padx=20, pady=20)
@@ -64,7 +74,7 @@ Button(root, text="9", command=lambda :get_variables(9), padx=5, pady=5).grid(ro
 
 # adding other buttons 0, +, _, *, /, =, and clear
 
-Button(root, text="AC", padx=4, pady=5).grid(row=5, column=0)
+Button(root, text="AC", command=lambda :clear_all(), padx=4, pady=5).grid(row=5, column=0)
 Button(root, text="0", command=lambda :get_variables(0), padx=5, pady=5).grid(row=5, column=1)
 Button(root, text="=", padx=5, pady=5).grid(row=5, column=2)
 
@@ -81,10 +91,11 @@ Button(root, text="(", command=lambda :get_variables("("), padx=6, pady=5).grid(
 Button(root, text="EXP", padx=3, pady=5).grid(row=5, column=4)
 
 
-Button(root, text="<-", padx=5, pady=5).grid(row=2, column=5)
+Button(root, text="<-", command=lambda :del_char(), padx=5, pady=5).grid(row=2, column=5)
 Button(root, text="x!", padx=5, pady=5).grid(row=3, column=5)
 Button(root, text=")", command=lambda :get_variables(")"), padx=6, pady=5).grid(row=4, column=5)
 Button(root, text="^2", padx=3, pady=5).grid(row=5, column=5)
+
 
 
 

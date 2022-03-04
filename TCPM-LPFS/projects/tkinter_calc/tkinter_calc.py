c18d694ab37db192ec4288ddd3970cfb7bb5175f
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-04 12:35:03 -0800

    finished tkinter_calc.py

--- TCPM-LPFS/projects/tkinter_calc/tkinter_calc.py
+++ TCPM-LPFS/projects/tkinter_calc/tkinter_calc.py
@@ -2,11 +2,14 @@
 # Date Created: 3/4/2022
 # Date Modified: 3/4/2022
 
-# Description:
+# Description: A GUI calculator in Python and tkinter.
 
 # Usage:
 
+
 from tkinter import *
+import math
+
 
 root = Tk()
 root.title("Calculator")
@@ -25,6 +28,31 @@ def del_char():
 	length = len(display.get())	
 	display.delete(length - 1)	
 
+def get_operation(operator):
+	global i
+	length = len(display.get())
+	display.insert(i, operator)
+	i += length
+
+def factorial():
+	#calculate the factorial and return the result.
+	num = display.get()
+	# math.factorial(num)
+	display.insert(i, math.factorial(int(num)))
+
+def calculate():
+	# calculate the result
+	equation = display.get()
+	# check if the equation has a solution
+	try:
+		answer = eval(equation)
+		clear_all()
+		display.insert(0, answer)
+	except:
+		clear_all()
+		display.insert(0, "Error")
+	
+	
 
 
 # adding the iput field
@@ -58,25 +86,25 @@ Button(root, text="9", command=lambda :get_variables(9), padx=5, pady=5).grid(ro
 
 Button(root, text="AC", command=lambda :clear_all(), padx=4, pady=5).grid(row=5, column=0)
 Button(root, text="0", command=lambda :get_variables(0), padx=5, pady=5).grid(row=5, column=1)
-Button(root, text="=", padx=5, pady=5).grid(row=5, column=2)
+Button(root, text="=", command=lambda :calculate(), padx=5, pady=5).grid(row=5, column=2)
 
-Button(root, text="+", padx=4, pady=5).grid(row=2, column=3)
-Button(root, text="-", padx=5, pady=5).grid(row=3, column=3)
-Button(root, text="*", padx=5, pady=5).grid(row=4, column=3)
-Button(root, text="/", padx=5, pady=5).grid(row=5, column=3)
+Button(root, text="+", command=lambda :get_operation("+"), padx=4, pady=5).grid(row=2, column=3)
+Button(root, text="-", command=lambda :get_operation("-"),padx=5, pady=5).grid(row=3, column=3)
+Button(root, text="*", command=lambda :get_operation("*"), padx=5, pady=5).grid(row=4, column=3)
+Button(root, text="/", command=lambda :get_operation("/"), padx=5, pady=5).grid(row=5, column=3)
 
 # adding other operators
 
-Button(root, text="Pi", padx=5, pady=5).grid(row=2, column=4)
-Button(root, text="%", padx=5, pady=5).grid(row=3, column=4)
+Button(root, text="Pi", command=lambda :get_operation("* 3.14"), padx=5, pady=5).grid(row=2, column=4)
+Button(root, text="%", command=lambda :get_operation("%"), padx=5, pady=5).grid(row=3, column=4)
 Button(root, text="(", command=lambda :get_variables("("), padx=6, pady=5).grid(row=4, column=4)
-Button(root, text="EXP", padx=3, pady=5).grid(row=5, column=4)
+Button(root, text="EXP", command=lambda :get_operation("**"), padx=3, pady=5).grid(row=5, column=4)
 
 
 Button(root, text="<-", command=lambda :del_char(), padx=5, pady=5).grid(row=2, column=5)
-Button(root, text="x!", padx=5, pady=5).grid(row=3, column=5)
+Button(root, text="x!", command=lambda :factorial(), padx=5, pady=5).grid(row=3, column=5)
 Button(root, text=")", command=lambda :get_variables(")"), padx=6, pady=5).grid(row=4, column=5)
-Button(root, text="^2", padx=3, pady=5).grid(row=5, column=5)
+Button(root, text="^2", command=lambda :get_operation("**2"), padx=3, pady=5).grid(row=5, column=5)
 
 
 

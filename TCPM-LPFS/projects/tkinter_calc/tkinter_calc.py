3ca17b5f28c6b9c3d439ac9f06153b827d51a07d
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-04 10:09:03 -0800

    updating tkinter_calc.py

--- TCPM-LPFS/projects/tkinter_calc/tkinter_calc.py
+++ TCPM-LPFS/projects/tkinter_calc/tkinter_calc.py
@@ -54,23 +54,28 @@ Button(root, text="9", padx=5, pady=5).grid(row=4, column=2)
 
 # adding other buttons 0, +, _, *, /, =, and clear
 
-Button(root, text="AC", padx=5, pady=5).grid(row=5, column=0)
+Button(root, text="AC", padx=4, pady=5).grid(row=5, column=0)
 Button(root, text="0", padx=5, pady=5).grid(row=5, column=1)
 Button(root, text="=", padx=5, pady=5).grid(row=5, column=2)
 
-Button(root, text="+", padx=5, pady=5).grid(row=2, column=3)
+Button(root, text="+", padx=4, pady=5).grid(row=2, column=3)
 Button(root, text="-", padx=5, pady=5).grid(row=3, column=3)
 Button(root, text="*", padx=5, pady=5).grid(row=4, column=3)
 Button(root, text="/", padx=5, pady=5).grid(row=5, column=3)
 
 Button(root, text="Pi", padx=5, pady=5).grid(row=2, column=4)
 Button(root, text="%", padx=5, pady=5).grid(row=3, column=4)
-Button(root, text="(*)", padx=5, pady=5).grid(row=4, column=4)
-Button(root, text="EXP", padx=5, pady=5).grid(row=5, column=4)
+Button(root, text="(", padx=6, pady=5).grid(row=4, column=4)
+Button(root, text="EXP", padx=3, pady=5).grid(row=5, column=4)
 
 # Button(root, text="", padx=20, pady=20).grid(row=4, column=0)
 # Button(root, text="", padx=20, pady=20).grid(row=4, column=1)
 # Button(root, text="", padx=20, pady=20).grid(row=4, column=2)
+
+Button(root, text="<-", padx=5, pady=5).grid(row=2, column=5)
+Button(root, text="x!", padx=5, pady=5).grid(row=3, column=5)
+Button(root, text=")", padx=6, pady=5).grid(row=4, column=5)
+Button(root, text="^2", padx=3, pady=5).grid(row=5, column=5)
 
 
 

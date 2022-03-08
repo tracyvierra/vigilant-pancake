f377eb1ba45fa19e1472f47fb08dba329a2f0876
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-07 16:42:50 -0800

    updated database_data_input_gui.py

--- TCPM-LPFS/examples/database_data_input_gui.py
+++ TCPM-LPFS/examples/database_data_input_gui.py
@@ -22,9 +22,44 @@ frame.place(relx=0.3, rely=0.1, relwidth=0.8, relheight=0.8)
 label = Label(frame, text="Student Database Data Input",)
 label.grid(row=0, column=1,)
 
+label = Label(frame, text="Name:")
+label.grid(row=1, column=0)
 
+label = Label(frame, text="Age:")
+label.grid(row=2, column=0)
 
+label = Label(frame, text="Address:")
+label.grid(row=3, column=0)
 
+entry_name = Entry(frame)
+entry_name.grid(row=1, column=1)
+
+entry_age = Entry(frame)
+entry_age.grid(row=2, column=1)
+
+entry_address = Entry(frame)
+entry_address.grid(row=3, column=1)
+
+button = Button(frame, text="Submit", command=lambda: get_data(entry_name.get(), entry_age.get(), entry_address.get()))
+button.grid(row=4, column=1)
+
+def get_data(name,age,address):
+	try:
+		conn = psycopg2.connect(database="studentdb", user="postgres", password="Tr1cky1!", host="localhost", port="5432")
+		cur = conn.cursor()
+		name = entry_name.get()
+		age = entry_age.get()
+		address = entry_address.get()
+
+		query = '''INSERT INTO new_students (NAME, AGE, ADDRESS) VALUES (%s, %s, %s);'''
+		cur.execute(query,(name,age,address))
+
+		conn.commit()
+		conn.close()
+		print("Record inserted successfully into new_students table")
+		print(" ")
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)	
 
 
 

6aaf35f315f5eb1ed83a946f6fa427e55a821a9b
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-07 17:38:10 -0800

    finished database_data_input_gui.py

--- TCPM-LPFS/examples/database_data_input_gui.py
+++ TCPM-LPFS/examples/database_data_input_gui.py
@@ -11,7 +11,7 @@ import tkinter as tk
 import psycopg2
 
 root = Tk()
-root.title("Database Data Input")
+root.title("Student Database - Data Input")
 # root.geometry("900x500")
 canvas = Canvas(root, width = 900, height = 500)
 canvas.pack()
@@ -53,7 +53,44 @@ label_search.grid(row=8, column=0)
 id_search = Entry(frame)
 id_search.grid(row=8, column=1)
 button_search = Button(frame, text="Search", command=lambda: search_data(id_search.get()))
-button_search.grid(row=9, column=1)
+button_search.grid(row=8, column=2)
+
+button_clear = Button(frame, text="Clear", command=lambda: clear_search())
+button_clear.grid(row=8, column=3)
+
+def clear_search():
+	id_search.delete(0, END)
+	
+
+
+
+def display_all():
+	try:
+		conn = psycopg2.connect(database="studentdb", user="postgres", password="Tr1cky1!", host="localhost", port="5432")
+		cur = conn.cursor()
+		query = '''SELECT * FROM new_students;'''
+		cur.execute(query)
+		rows = cur.fetchall()
+		label = Label(frame, text="All Students:")
+		label.grid(row=10, column=1)
+		list_box_all = Listbox(frame, width=50, height=30)
+		list_box_all.grid(row=11, column=1, columnspan=3)
+		for x in rows:
+			list_box_all.insert(END, x)
+		conn.commit()
+		conn.close()
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+
+def display_search(row):
+	list_box_search = Listbox(frame, width=50, height=5)
+	list_box_search.grid(row=9, column=1, columnspan=3)
+	list_box_search.insert(END, "ID: " + str(row[0]))
+	list_box_search.insert(END, "Name: " + str(row[1]))
+	list_box_search.insert(END, "Age: " + str(row[2]))
+	list_box_search.insert(END, "Address: " + str(row[3]))
+
+
 
 
 def search_data(id):
@@ -64,6 +101,7 @@ def search_data(id):
 		query = '''SELECT * FROM new_students WHERE ID = %s;'''
 		cur.execute(query,(id,))
 		row = cur.fetchone()
+		display_search(row)
 		print(" ")
 		print("ID: ", row[0])
 		print("Name: ", row[1])
@@ -96,13 +134,12 @@ def get_data(name,age,address):
 
 		conn.commit()
 		conn.close()
+		display_all()
 		print("Record inserted successfully into new_students table")
 		print(" ")
 	except (Exception, psycopg2.DatabaseError) as error:
 		print(error)	
 
 
-
+display_all()
 root.mainloop()
-
-

d15df699957ffffe4508c7eca2732d29821a202a
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-07 16:47:16 -0800

    updates to files

--- TCPM-LPFS/examples/database_data_input.py
+++ TCPM-LPFS/examples/database_data_input.py
@@ -1,6 +1,6 @@
 # Author: Tracy Vierra
 # Date Created: 3/4/2022
-# Date Modified: 3/4/2022
+# Date Modified: 3/7/2022
 
 # Description:
 
--- TCPM-LPFS/examples/database_data_input_gui.py
+++ TCPM-LPFS/examples/database_data_input_gui.py
@@ -43,6 +43,15 @@ entry_address.grid(row=3, column=1)
 button = Button(frame, text="Submit", command=lambda: get_data(entry_name.get(), entry_age.get(), entry_address.get()))
 button.grid(row=4, column=1)
 
+button_clear = Button(frame, text="Clear", command=lambda: clear_data())
+button_clear.grid(row=5, column=1)
+
+def clear_data():
+	entry_name.delete(0, END)
+	entry_age.delete(0, END)
+	entry_address.delete(0, END)
+
+
 def get_data(name,age,address):
 	try:
 		conn = psycopg2.connect(database="studentdb", user="postgres", password="Tr1cky1!", host="localhost", port="5432")
@@ -64,55 +73,5 @@ def get_data(name,age,address):
 
 
 root.mainloop()
-
-
-
-
-
-
-# def create_table():
-# 	try:
-# 		conn = psycopg2.connect(database="studentdb", user="postgres", password="Tr1cky1!", host="localhost", port="5432")
-# 		cur = conn.cursor()
-# 		cur.execute('''CREATE TABLE new_students(ID SERIAL, NAME TEXT, AGE TEXT, ADDRESS TEXT);''')
-
-# 		conn.commit()
-# 		conn.close()
-# 		print("Table created successfully in PostgreSQL ")
-# 	except (Exception, psycopg2.DatabaseError) as error:
-# 		print(error)
-		
-# def insert_data():
-# 	conn = psycopg2.connect(database="studentdb", user="postgres", password="Tr1cky1!", host="localhost", port="5432")
-# 	cur = conn.cursor()
-# 	name = input("Enter name: ")
-# 	age = input("Enter age: ")
-# 	address = input("Enter address: ")
-
-# 	query = '''INSERT INTO new_students (NAME, AGE, ADDRESS) VALUES (%s, %s, %s);'''
-# 	cur.execute(query,(name,age,address))
-
-# 	conn.commit()
-# 	conn.close()
-# 	print("Record inserted successfully into new_students table")
-# 	print(" ")
-
-
-
-
-# while True:
-# 	print("1. Create table")
-# 	print("2. Insert data")
-# 	print("3. Exit")
-# 	print(" ")
-# 	choice = int(input("Enter choice: "))
-# 	if choice == 1:
-# 		create_table()
-# 	elif choice == 2:
-# 		insert_data()
-# 	elif choice == 3:
-# 		break
-# 	else:
-# 		print("Invalid choice")
 
 

a76e56d84fcc1e1d9cdf3cd10e43e8c6b9e6500b
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-10 15:44:05 -0800

    updated compress.py

--- TCPM-LPFS/pypro/compress.py
+++ TCPM-LPFS/pypro/compress.py
@@ -5,19 +5,62 @@
 # Description:
 
 # Usage:
+
+
 import zlib, base64
 
+def compress(input_file, output_file):
+	data = open(input_file, 'r').read()
+	b_data = bytes(data, 'utf-8')
+	compressed_data = base64.b64encode(zlib.compress(b_data,9))		
+	compressed_file = open(output_file, 'w').write(compressed_data.decode('utf-8'))	
+	return compressed_file
+
+def decompress(input_file, output_file):
+	compressed_data = open(input_file, 'r').read()
+	decompressed_data = zlib.decompress(base64.b64decode(compressed_data))
+	decompressed_file = open(output_file, 'w').write(decompressed_data.decode('utf-8'))
+	return decompressed_file
 
 
-data = open("demo.txt", 'r').read()
-print(data)
-b_data = bytes(data, 'utf-8')
-compressed_data = base64.b64encode(zlib.compress(b_data,9))		
-print(compressed_data)
-compressed_file = open("compressed.txt", 'w').write(compressed_data.decode('utf-8'))	
-print(compressed_file)
+print("1. Compress file ")
+print("2. Decompress file ")
+print("3. Exit ")
 
-decompressed_data = zlib.decompress(base64.b64decode(compressed_data))
-print(decompressed_data)
+mode = input("Enter 1, 2, or 3: ")
+
+if mode == "1":
+	input_file = input("Enter the name of the file to compress: ")
+	output_file = input("Enter the name of the file to save the compressed data: ")
+	compressed_file = compress(input_file, output_file)
+	print("Compressed file: ", compressed_file)
+elif mode == "2":
+	input_file = input("Enter the name of the file to decompress: ")
+	output_file = input("Enter the name of the file to save the decompressed data: ")
+	decompressed_file = decompress(input_file, output_file)
+	print("Decompressed file: ", decompressed_file)
+elif mode == "3":
+	print("Exiting")
+	exit()
+else:
+	print("Invalid input")
+	exit()
+
+
+
+
+
+# import zlib, base64
+
+# data = open("demo.txt", 'r').read()
+# print(data)
+# b_data = bytes(data, 'utf-8')
+# compressed_data = base64.b64encode(zlib.compress(b_data,9))		
+# print(compressed_data)
+# compressed_file = open("compressed.txt", 'w').write(compressed_data.decode('utf-8'))	
+# print(compressed_file)
+
+# decompressed_data = zlib.decompress(base64.b64decode(compressed_data))
+# print(decompressed_data)
 
 

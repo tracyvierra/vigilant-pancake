37a389688605de9edf5fa153171e81249832ba09
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-10 13:16:24 -0800

    updated compress.py and added compressed.txt

--- TCPM-LPFS/pypro/compress.py
+++ TCPM-LPFS/pypro/compress.py
@@ -5,6 +5,17 @@
 # Description:
 
 # Usage:
+import zlib, base64
 
-data = open('demo.txt', 'r').read()
+
+
+data = open("demo.txt", 'r').read()
+print(data)
+b_data = bytes(data, 'utf-8')
+compressed_data = base64.b64encode(zlib.compress(b_data,9))		
+print(compressed_data)
+compressed_file = open("compressed.txt", 'w').write(compressed_data.decode('utf-8'))	
+print(compressed_file)
+
+
 

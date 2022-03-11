e325dce3d8d49c91e9566e9f7ea4d01b706669e1
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-11 13:50:00 -0800

    added text_2_speech_live.py

---
+++ TCPM-LPFS/text-2-speech/text_2_speech_live.py
@@ -0,0 +1,43 @@
+# Author: Tracy Vierra
+# Date Created: 3/11/2022
+# Date Modified: 3/11/2022
+
+# Description: Text to speech example using user input to speech and saved as a file.
+
+# Usage:
+
+from gtts import gTTS
+import os
+import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter import *
+
+
+# text = open('demo.txt', 'r').read()
+
+# gTTS(text=text, lang='en').save("hello.mp3")
+# os.system("start hello.mp3")
+
+def convert(entry, output_file):
+    text = entry
+    tts = gTTS(text=text, lang='en')
+    tts.save(output_file)
+
+output_file = "output.mp3"
+
+root = Tk()
+root.title("Text 2 Speech")
+canvas1 = tk.Canvas(root, width = 300, height = 100)
+canvas1.pack()
+
+entry = tk.Entry(root)
+canvas1.create_window(150, 20, window=entry)
+canvas1.pack()
+
+button_convert = tk.Button(text='Convert', command= lambda:convert(entry.get(), output_file))
+button_convert.configure(background='green', foreground='white')
+canvas1.create_window(150, 60, window=button_convert)
+canvas1.pack()
+
+
+root.mainloop()

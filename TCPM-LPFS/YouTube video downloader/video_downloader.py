1b763ca856dd252987c1c91c61c11e5a7e95dbfc
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-15 10:50:28 -0700

    updated video_downloader.py

--- TCPM-LPFS/YouTube video downloader/video_downloader.py
+++ TCPM-LPFS/YouTube video downloader/video_downloader.py
@@ -6,18 +6,35 @@
 
 # Usage:
 
+# from sysconfig import get_path
 import moviepy as mp
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
+from pytube import YouTube
+from moviepy.editor import *
 
 
+
+def download_video():
+	video_path = url_entry.get()
+	download_path = path_label.cget("text")
+	mp4 = YouTube(video_path).streams.get_highest_resolution().download(download_path)
+	video_clip = VideoFileClip(mp4)
+	video_clip.close()
+	messagebox.showinfo("Download Complete", "Video downloaded to " + download_path)
+
+    
+
+def get_path():
+    path = filedialog.askdirectory()
+    path_label.config(text=path)
 
 root = tk.Tk()
 root.title("YouTube Video Downloader")
-root.geometry("500x500")
+root.geometry("400x350")
 
 canvas = tk.Canvas(root, height=400, width=300)
 canvas.pack()
@@ -32,10 +49,16 @@ url_entry = tk.Entry(root, width=50)
 canvas.create_window(150, 80, window=url_label)
 canvas.create_window(150, 100, window=url_entry)
 
+# path to download videos
+path_label = tk.Label(root, text = "Select path to download videos:" ) 
+path_button = tk.Button(root, text="Browse", command=get_path)
+canvas.create_window(150, 150, window=path_label)
+canvas.create_window(150, 180, window=path_button)
 
 
-
-
+# download button
+download_button = tk.Button(root, text="Download", command=lambda: download_video())
+canvas.create_window(150, 250, window=download_button)
 
 
 

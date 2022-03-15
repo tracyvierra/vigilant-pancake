485d3399fa526d0ac10617c3935b4bbc61cbf6d0
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-15 13:13:24 -0700

    updated CC_number_verify.py

--- TCPM-LPFS/CC number verification/CC_number_verify.py
+++ TCPM-LPFS/CC number verification/CC_number_verify.py
@@ -2,7 +2,26 @@
 # Date Created: 3/15/2022
 # Date Modified: 3/15/2022
 
-# Description: CC number verification program
+# Description: CC number verification program using the Luhn Algorithm
 
 # Usage:
 
+# example: 5610591081018250
+card_num = input("Enter a credit card number: ")
+odd_sum = 0
+double_list = []
+even_sum = 0
+number = list(card_num)
+
+for (idx, val) in enumerate(number):
+	if idx % 2 != 0:
+		odd_sum += int(val)
+	else:
+		double_list.append(int(val) * 2)
+		
+		
+
+print(double_list)
+print(odd_sum)
+
+	

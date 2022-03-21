ad1bece442d194ff07d11716d1d8089a1c8e8451
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-21 16:47:58 -0700

    added open_excel.py to ATBS folder

--- ATBS/open_execl.py
+++ ATBS/open_execl.py
@@ -6,6 +6,24 @@
 
 # Usage:
 
+from termios import B1000000
 import openpyxl
 import os
+
+os.chdir('C:\\Users\\tracy\Documents\\Python Scripts\\git\\vigilant-pancake\\ATBS')
+
+workbook = openpyxl.load_workbook('example.xlsx')
+sheet = workbook.get_sheet_by_name('Sheet1')
+# type(sheet)
+
+# class 'openpyxl.worksheet.worksheet.Worksheet'
+workbook.get_sheet_names()
+
+cell = sheet['A1']
+cell.value
+print(str(cell.value))
+print(str(sheet['A1'].value))	# if you don't want to save the cell 
+print(str(sheet['B1'].value))		
+print(str(sheet['C1'].value))
+
 

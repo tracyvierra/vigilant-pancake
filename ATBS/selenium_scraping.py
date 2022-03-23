a3bcb4a4cf0659062ba22e2caa6bdce2ef5f8267
Tracy Vierra <tracyvierra@yahoo.com>
2022-03-23 09:20:02 -0700

    adding selenium_scraping.py

---
+++ ATBS/selenium_scraping.py
@@ -0,0 +1,12 @@
+# Author: Tracy Vierra
+# Date Created: 3/22/2022
+# Date Modified: 3/22/2022
+
+# Description:
+
+# Usage:
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+# browser.get('https://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994/')

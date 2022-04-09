4885597cde6b5566c71e19391525e773b5761c9d
Tracy Vierra <tracyvierra@yahoo.com>
2022-04-08 17:23:21 -0700

    WIP jarvis-black.py - removed commented out code, logging not needed

--- Jarvis 2.0/test.py
+++ Jarvis 2.0/test.py
@@ -1,1 +1,19 @@
 https://api.openweathermap.org/data/2.5/weather?&units=standard&q=chelan&appid=cce9b0c81b54033cc50f4e071fc11360
+
+
+# start logging file for session:
+# x = datetime.datetime.now()
+# file_name = x.strftime("%Y-%m-%d-%H-%M-%S")
+# file_handler = logging.FileHandler(
+#     filename="jarvis_error_log-" + file_name + ".log", mode="w"
+# )
+# handlers = [file_handler]
+
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
+#     handlers=handlers,
+# )
+
+# logger = logging.getLogger("LOGGER_NAME")
+

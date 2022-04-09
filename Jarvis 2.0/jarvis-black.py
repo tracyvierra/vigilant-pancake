46136e4c6fd58817ceaf5e7a9ef2f8b739ca68e9
Tracy Vierra <tracyvierra@yahoo.com>
2022-04-09 16:46:18 -0700

    WIP jarvis-black.py - added adjustment for ambient noise. adding test files for reference.

--- Jarvis 2.0/jarvis-black.py
+++ Jarvis 2.0/jarvis-black.py
@@ -96,6 +96,7 @@ def takeCommandCMD():
 def takeCommandMIC():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
--- Jarvis 2.0/test.py
+++ Jarvis 2.0/test.py
@@ -1,4 +1,4 @@
-https://api.openweathermap.org/data/2.5/weather?&units=standard&q=chelan&appid=cce9b0c81b54033cc50f4e071fc11360
+# https://api.openweathermap.org/data/2.5/weather?&units=standard&q=chelan&appid=cce9b0c81b54033cc50f4e071fc11360
 
 
 # start logging file for session:
@@ -17,3 +17,24 @@ https://api.openweathermap.org/data/2.5/weather?&units=standard&q=chelan&appid=c
 
 # logger = logging.getLogger("LOGGER_NAME")
 
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)  # listen for 1 second to calibrate the energy threshold for ambient noise levels
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))

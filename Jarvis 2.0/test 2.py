c852d4cc996b2783f808e62907e04ce5e6a085be
Tracy Vierra <tracyvierra@yahoo.com>
2022-04-09 18:30:18 -0700

    commented out code in test 2.py

--- Jarvis 2.0/test 2.py
+++ Jarvis 2.0/test 2.py
@@ -1,21 +1,21 @@
 # python code to use openweatherAPI to get current weather and temperature in Fahrenheit:
 
 
-def open_check_weather():
-    try:
-        speak("What is your zip code?")
-        zip_code = takeCommandMIC()
-        url = OW_API_LINK + zip_code + APPID + "&units=imperial"
-        res = requests.get(url)
-        data = res.json()
-        weather = data["weather"][0]["main"]
-        temp = data["main"]["temp"]
-        temp = round(temp, 2)
-        description = data["weather"][0]["description"]
-        print(temp)
-        print(weather)
-        speak("The temperature is" + str(temp) + " degrees")
-        speak("The weather is " + description)
-    except Exception as e:
-        print(e)
-        print("Failed to check weather")
+# def open_check_weather():
+#     try:
+#         speak("What is your zip code?")
+#         zip_code = takeCommandMIC()
+#         url = OW_API_LINK + zip_code + APPID + "&units=imperial"
+#         res = requests.get(url)
+#         data = res.json()
+#         weather = data["weather"][0]["main"]
+#         temp = data["main"]["temp"]
+#         temp = round(temp, 2)
+#         description = data["weather"][0]["description"]
+#         print(temp)
+#         print(weather)
+#         speak("The temperature is" + str(temp) + " degrees")
+#         speak("The weather is " + description)
+#     except Exception as e:
+#         print(e)
+#         print("Failed to check weather")
